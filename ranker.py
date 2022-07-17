936c558935de1b47b650b16b3d162e450528d9b4
Bhanvi Sachdev <60591765+BhanviSachdev@users.noreply.github.com>
2022-07-18 01:20:08 +0530

    Update ranker.py

--- ranker.py
+++ ranker.py
@@ -21,7 +21,7 @@ def main():
     
     flag = sys.argv[1]
 
-    corpus = read(encoding=('ISO-8859-1'),low_memory=False)
+    corpus = read("utf-16")
 
     if (flag == "-n"):
         print("------------------------------------------------")
